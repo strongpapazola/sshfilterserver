f7c08f484f2f0fab608855aef083a0c7c59f37fa
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2021-01-06 06:58:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ iptables -D INPUT -j SSHFILTER
 iptables -F SSHFILTER
 mkdir -p /opt/sshfilter/
 apt install curl -y
-curl http://exppar.bisaai.id/sshfilter.sh -o /opt/sshfilter/sshfilter.sh
+curl http://system.systar.my.id/sshfilter.sh -o /opt/sshfilter/sshfilter.sh
 chmod +x /opt/sshfilter/sshfilter.sh
-curl http://exppar.bisaai.id/sshfilter.service -o /etc/systemd/system/sshfilter.service
+curl http://system.systar.my.id/sshfilter.service -o /etc/systemd/system/sshfilter.service
 systemctl enable sshfilter.service
 systemctl restart sshfilter.service
 systemctl daemon-reload
 systemctl restart sshfilter.service
-#wget -qO- http://exppar.bisaai.id/install.sh | bash -s --
+#wget -qO- http://system.systar.my.id/install.sh | bash -s --
